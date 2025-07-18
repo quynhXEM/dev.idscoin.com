b2ecd7803f24fcd70be910878f8caa8eb30203de
QuynhXEM <quynh@xem.edu.vn>
2025-07-18 11:53:15 +0700

    Token

--- src/app/api/directus/request/route.ts
+++ src/app/api/directus/request/route.ts
@@ -29,46 +29,31 @@ export const POST = async (request: Request) => {
           break;
         case "readItem":
           res = await directus.request(
-            withToken(
-              (admin ? APP_TOKEN : access_token) || "",
-              readItem(collection, id, params)
-            )
+            withToken(APP_TOKEN, readItem(collection, id, params))
           );
           break;
 
         case "readItems":
           res = await directus.request(
-            withToken(
-              (admin ? APP_TOKEN : access_token) || "",
-              readItems(collection, params)
-            )
+            withToken(APP_TOKEN, readItems(collection, params))
           );
           break;
 
         case "updateItem":
           res = await directus.request(
-            withToken(
-              (admin ? APP_TOKEN : access_token) || "",
-              updateItem(collection, id, items)
-            )
+            withToken(APP_TOKEN, updateItem(collection, id, items))
           );
           break;
 
         case "createItem":
           res = await directus.request(
-            withToken(
-              (admin ? APP_TOKEN : access_token) || "",
-              createItem(collection, items, params)
-            )
+            withToken(APP_TOKEN, createItem(collection, items, params))
           );
           break;
 
         case "deleteItem":
           res = await directus.request(
-            withToken(
-              (admin ? APP_TOKEN : access_token) || "",
-              deleteItem(collection, items)
-            )
+            withToken(APP_TOKEN, deleteItem(collection, items))
           );
           break;
       }
@@ -81,7 +66,7 @@ export const POST = async (request: Request) => {
     return NextResponse.json({
       error: "DIRECTUS_REQUEST_ERROR",
     });
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     if (process.env.NODE_ENV === "development") {
       console.error("Error API directus:", error);
@@ -97,4 +82,3 @@ export const POST = async (request: Request) => {
     });
   }
 };
-
